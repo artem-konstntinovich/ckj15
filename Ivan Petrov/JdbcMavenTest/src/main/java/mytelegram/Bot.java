71b0e72d567c32ebdea244de96aad8da1aa941d4
Artem K. Koltunov <artem.chistyakov@gmail.com>
2021-12-27 13:11:57 +0200

    add tokens

--- Ivan Petrov/JdbcMavenTest/src/main/java/mytelegram/Bot.java
+++ Ivan Petrov/JdbcMavenTest/src/main/java/mytelegram/Bot.java
@@ -1,8 +1,12 @@
 package mytelegram;
 
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.generics.LongPollingBot;
+
+import java.util.Date;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -13,16 +17,60 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        //
+        if (   update != null //пустота от другого - бота - программы
+                && update.hasMessage()
+                && update.getMessage().hasText())
+        {
+            String message_text = update.getMessage().getText();
+            if (message_text.equals("/time"))
+            {
+                Date currentDate = new Date();
+                String messageSend = currentDate.toString();
 
+                long chat_id = update.getMessage().getChatId();
+                sendMsg(update.getMessage().
+                        getChatId().toString(), messageSend);
+            }
+            else if (message_text.equals("/help"))
+            {
+                long chat_id = update.getMessage().getChatId();
+                sendMsg(update.getMessage().
+                        getChatId().toString(), "две команды: /time и /help /empl");
+            }
+            else//в ответ отправл¤ю эхом то, что пришло
+            {
+                String message = update.getMessage().getText();
+                sendMsg(update.getMessage().getChatId().toString(), message);
+            }
+        }
+        //
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return "mytelega2021122700bot";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return "5034197038:AAEtAgGaXZ7A8bQBUyJWQqHEEV0bLXRX87c";
     }
+    /**
+     * ћетод дл¤ настройки сообщени¤ и его отправки.
+     * @param chatId id чата
+     * @param s —трока, которую необходимот отправить в качестве сообщени¤.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            //log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
+
 }
